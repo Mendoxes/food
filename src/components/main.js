47b21185909ea544735efb326aecb2c64d1ac9a3
Sebastian <seba.lewy@op.pl>
2021-10-30 14:19:26 +0100

    Add stuff

--- src/components/main.js
+++ src/components/main.js
@@ -2,19 +2,31 @@ import React from 'react'
 import hero from "../hero.jpg"
 import '../Main.css';
 import vid from "../vid.mp4"
+import Fade from 'react-reveal/Fade';
 
 export default function main() {
+    
     return (
         <div className="heroDiv" >
-            <img className="heroImg" src={hero} />
-            <div>
+            <div className="rela">
+           <Fade delay="500"> <p className="text-on-image"> Food is everything</p></Fade>
+            <Fade delay="200"><img className="heroImg" src={hero} /></Fade>
+            
+            </div>
+          <Fade> 
+              
+              <h1 className="h1Prop">You are what you eat</h1>
+               <div className="vid">
 
-            <video width="320" height="240"  loop autoPlay muted>
+            <video width="520" height="440"  loop autoPlay muted>
   <source src={vid} type="video/mp4"/>
  
-  Your browser does not support the video tag.
+ 
 </video>
-            </div>
+
+
+<p>"You are what you eat" that is profound sentence that everybody should take seriously</p>
+            </div></Fade>
         </div>
     )
 }
