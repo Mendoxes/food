256ccb72d1e96fa7cbe6d275884ea04a839e9b20
Sebastian <seba.lewy@op.pl>
2021-10-30 13:04:32 +0100

    change img

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
+import NavBar from "./components/NavBar.js"
 import './App.css';
 import Hero from "./components/Hero.js"
 import Main from "./components/main.js"
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Main></Main>
      <Hero></Hero>
     </div>
